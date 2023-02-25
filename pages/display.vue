b77da3b80b0608c7d8222b0e695da394fe86fd4e
developersth <kritsadee.satewin@gmail.com>
2023-02-26 01:42:16 +0700

    first commit

---
+++ pages/display.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <table
+      id="example"
+      class="table table-striped table-bordered"
+      style="width: 100%"
+    >
+      <thead class="thead-dark">
+        <tr>
+          <th class="text-center"><b-button size="lg">#</b-button></th>
+          <th class="text-center">
+            <b-button block size="lg" variant="success">BAY 1</b-button>
+          </th>
+          <th class="text-center">
+            <b-button block size="lg" variant="success">BAY 2</b-button>
+          </th>
+          <th class="text-center">
+            <b-button block size="lg" variant="danger">BAY 3</b-button>
+          </th>
+          <th class="text-center">
+            <b-button block size="lg" variant="success">BAY 4</b-button>
+          </th>
+          <th class="text-center">
+            <b-button block size="lg" variant="success">BAY 5</b-button>
+          </th>
+          <th class="text-center">
+            <b-button block size="lg" variant="danger">BAY 6A</b-button>
+          </th>
+          <th class="text-center">
+            <b-button block size="lg" variant="success">BAY 6B</b-button>
+          </th>
+          <th class="text-center">
+            <b-button block size="lg" variant="success">BAY 7</b-button>
+          </th>
+          <th class="text-center">
+            <b-button block size="lg" variant="primary">รับตั๋ว</b-button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>กำลังโหลด</td>
+          <td>
+            <div class="card bg-secondary text-white">
+              <div class="card-body" variant="primary">
+                <h5 class="card-title">75-7576</h5>
+                <p class="card-text">นายมนัส บุญจำนง</p>
+                <a href="#" class="btn btn-danger">กำลังเติม</a>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="card bg-dark text-white">
+              <div class="card-body">        <h5 class="card-title">75-7576</h5>
+                <p class="card-text">นายกฤษดี สาเทวิน</p>
+                <a href="#" class="btn btn-danger">กำลังเติม</a></div>
+            </div>
+          </td>
+          <td>
+            <div class="card bg-secondary text-white">
+              <div class="card-body" variant="primary">
+                <h5 class="card-title">75-7576</h5>
+                <p class="card-text">นายมนัส บุญจำนง</p>
+                <a href="#" class="btn btn-danger">กำลังเติม</a>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="card bg-secondary text-white">
+              <div class="card-body" variant="primary">
+                <h5 class="card-title">75-7576</h5>
+                <p class="card-text">นายมนัส บุญจำนง</p>
+                <a href="#" class="btn btn-danger">กำลังเติม</a>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="card bg-secondary text-white">
+              <div class="card-body" variant="primary">
+                <h5 class="card-title">75-7576</h5>
+                <p class="card-text">นายมนัส บุญจำนง</p>
+                <a href="#" class="btn btn-danger">กำลังเติม</a>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="card bg-secondary text-white">
+              <div class="card-body" variant="primary">
+                <h5 class="card-title">75-7576</h5>
+                <p class="card-text">นายมนัส บุญจำนง</p>
+                <a href="#" class="btn btn-danger">กำลังเติม</a>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="card bg-secondary text-white">
+              <div class="card-body" variant="primary">
+                <h5 class="card-title">75-7576</h5>
+                <p class="card-text">นายมนัส บุญจำนง</p>
+                <a href="#" class="btn btn-danger">กำลังเติม</a>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="card bg-secondary text-white">
+              <div class="card-body" variant="primary">
+                <h5 class="card-title">75-7576</h5>
+                <p class="card-text">นายมนัส บุญจำนง</p>
+                <a href="#" class="btn btn-danger">กำลังเติม</a>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="card bg-info text-white">
+              <div class="card-body" variant="primary">
+                <h5 class="card-title">75-7576</h5>
+                <p class="card-text">นายมนัส บุญจำนง</p>
+                <a href="#" class="btn btn-danger">กำลังเติม</a>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+  <script>
+export default {
+  data() {
+    return {
+      items: [
+        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+        { age: 38, first_name: 'Jami', last_name: 'Carney' },
+      ],
+    }
+  },
+}
+</script>
+<style></style>
